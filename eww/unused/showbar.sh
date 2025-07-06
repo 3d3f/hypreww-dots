9044cb4ac6bb88d3f9f0a9a4b6f706b2d0024c64
3d3f <dbfside@gmail.com>
2025-07-06 12:05:44 +0200

    publicdots

--- eww/unused/showbar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$(eww get bar_visible)" == "false" ]]; then
-    eww open bar
-    eww update bar_hover=true
-    eww close hide-bar
-else
-    echo "Barra già visibile."
-fi
--- eww/unused/workspaces.sh
+++
@@ -1,5 +0,0 @@
-hyprctl workspaces -j | jq -c 'map({id, name, urgent})'
-
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-  hyprctl workspaces -j | jq -c 'map({id, name, urgent}) | sort_by(.id)'
-done
